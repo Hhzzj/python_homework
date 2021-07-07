b13a57685f1a38979e31fb0c84a81b2daddcd51d
Hhzzj <85665181+Hhzzj@users.noreply.github.com>
2021-07-07 14:25:00 +0800

    Update Josephus_problem.py

--- Josephus_problem.py
+++ Josephus_problem.py
@@ -1,9 +1,13 @@
 def Josephus_problem(people_nums, death_gap):
+    death_gap=death_gap%people_nums
     people_list = list(range(1, people_nums+1))
     death_list = []
     death_index = 0
 
-    while(len(people_list)):
+    if people_nums==1 or death_gap==1:
+        death_list = people_list
+
+    while(len(people_list)>1):
         death_index = (death_index+death_gap-1) % len(people_list)
         death_list.append(people_list.pop(death_index))
     print(death_list)
